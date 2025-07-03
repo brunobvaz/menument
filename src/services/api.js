3243574583a898de735a72e025ceea91e0c3f60c
brunobvaz@gmail.com <bbv19008>
2025-07-03 18:23:27 +0100

    frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://localhost:3001/api',
-  baseURL: '/api',
+  baseURL: '/api', // NGINX irá redirecionar para o backend
   headers: {
     'Content-Type': 'application/json',
   },
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  withCredentials: true, // necessário para enviar e receber cookies (sessão)
 });
 
 export default api;
+
